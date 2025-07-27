4d78d7f6b0f04afdb2ef1481cf8ef9c0a30e0c42
gahgsp <kelvinbianchi@gmail.com>
2025-07-27 11:37:49 +0200

    making shelf header close to original version

--- src/components/CargoHeader/CargoHeader.tsx
+++ src/components/CargoHeader/CargoHeader.tsx
@@ -1,49 +1,84 @@
 import { Box, Typography } from "@mui/material"
+import CalendarViewWeekOutlinedIcon from '@mui/icons-material/CalendarViewWeekOutlined';
 
 const styles = {
-    container: {
-        marginBottom: '2rem',
-        color: 'white'
-    },
-    informationHeader: {
-        height: '30px',
-        display: 'flex',
-        alignItems: 'center'
-    },
-    subtitle: {
-        height: '30px',
-        backgroundColor: '#6a79ab',
-        display: 'flex',
-        flexGrow: 1,
-        alignItems: 'center'
-    },
-    amount: {
-        width: '80px',
-        height: '30px',
-        backgroundColor: '#485054',
-        borderStyle: 'solid',
-        borderWidth: '2px',
-        borderColor: '#4f5869',
-        display: 'flex',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end'
-    }
+  container: {
+    marginBottom: '1rem',
+    color: 'white',
+    paddingRight: '16px',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    paddingLeft: '16px',
+    marginTop: '4px'
+  },
+  informationContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexGrow: 1,
+    backgroundColor: '#6a79ab'
+  },
+  iconContainer: {
+    height: '32px',
+    width: '32px',
+    display: 'flex',
+    alignItems: 'center',
+    borderTopLeftRadius: '4px',
+    borderBottomRightRadius: '4px'
+  },
+  icon: {
+    transform: 'rotate(90deg)',
+    marginLeft: '4px'
+  },
+  informationHeader: {
+    height: '32px',
+    display: 'flex',
+    alignItems: 'center',
+    flexGrow: 1,
+    borderLeft: '2px solid #485054'
+  },
+  subtitle: {
+    height: '32px',
+    display: 'flex',
+    flexGrow: 1,
+    alignItems: 'center',
+    paddingLeft: '16px',
+    paddingTop: '4px',
+    clipPath: 'polygon(0 0, 100% 0, calc(100% - 16px) 100%, 0 100%)',
+  },
+  amount: {
+    width: '80px',
+    height: '32px',
+    backgroundColor: '#485054',
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    paddingRight: '8px',
+    clipPath: 'polygon(16px 0, 100% 0, 100% 100%, 0 100%)',
+  }
 }
 
 const CargoHeader = () => {
-    return (
-        <Box sx={styles.container}>
-            <Typography variant="h2">Cargo Pick-Up</Typography>
-            <Box style={styles.informationHeader}>
-                <Box style={styles.subtitle}>
-                    <Typography variant="h5">Cargo Shelf</Typography>
-                </Box>
-                <Box style={styles.amount}>
-                    <Typography variant="h5">x34</Typography>
-                </Box>
-            </Box>
+  return (
+    <Box sx={styles.container}>
+      <Typography variant="h3" sx={styles.title}>Cargo Pick-Up</Typography>
+      <Box sx={styles.informationContainer}>
+        <Box sx={styles.iconContainer}>
+          <CalendarViewWeekOutlinedIcon sx={styles.icon} />
         </Box>
-    )
+        <Box style={styles.informationHeader}>
+          <Box style={styles.subtitle}>
+            <Typography component="div">Cargo Shelf</Typography>
+          </Box>
+          <Box style={styles.amount}>
+            <Typography component="div">x34</Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box >
+  )
 }
 
 export default CargoHeader
