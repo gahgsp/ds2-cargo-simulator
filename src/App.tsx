5468a7bf15568b8af5ea84be7b68aa52a2c53aa0
gahgsp <kelvinbianchi@gmail.com>
2025-08-07 16:29:05 +0200

    changing assets to public to work on deployed version

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,16 @@ const App = () => {
           'Samples of fish recovered from a BT area. It is possible that these creatures crossed over from the other side at the same time as the BTs.',
         size: 'M',
         weight: 4,
-        image: 'src/assets/img/fish-cargo.png',
+        image: '/img/fish-cargo.png',
         deliveryTo: 'Villa Libre'
       })),
     },
   ];
 
   const lostCargos: Cargo[] = [
-    { id: 69, title: 'Vinyl Record with Sounds of Nature', description: 'A red and circular vinyl record. Used to store sounds and songs in a reproducible way. This object was very famous before the Death Stranding.', size: 'S', weight: 2, image: 'src/assets/img/red-disc.png', deliveryTo: 'Musician' },
-    { id: 68, title: 'Pre-Death Stranding Collection of Songs', description: 'A yellow and circular vinyl record. Used to store sounds and songs in a reproducible way. This object was very famous before the Death Stranding.', size: 'S', weight: 2, image: 'src/assets/img/yellow-disc.png', deliveryTo: 'Musician' },
-    { id: 67, title: 'Jar of Doce de Leite (Mumu)', description: 'Whether to eat it as a dessert or together with bread, this sweet sauce-like confection is a gift sent from the other side. It is said: a spoon of Doce de Leite a day, keeps the timefall away.', size: 'L', weight: 5, image: 'src/assets/img/mumu-cargo.png', deliveryTo: 'Architect' }
+    { id: 69, title: 'Vinyl Record with Sounds of Nature', description: 'A red and circular vinyl record. Used to store sounds and songs in a reproducible way. This object was very famous before the Death Stranding.', size: 'S', weight: 2, image: '/img/red-disc.png', deliveryTo: 'Musician' },
+    { id: 68, title: 'Pre-Death Stranding Collection of Songs', description: 'A yellow and circular vinyl record. Used to store sounds and songs in a reproducible way. This object was very famous before the Death Stranding.', size: 'S', weight: 2, image: '/img/yellow-disc.png', deliveryTo: 'Musician' },
+    { id: 67, title: 'Jar of Doce de Leite (Mumu)', description: 'Whether to eat it as a dessert or together with bread, this sweet sauce-like confection is a gift sent from the other side. It is said: a spoon of Doce de Leite a day, keeps the timefall away.', size: 'L', weight: 5, image: '/img/mumu-cargo.png', deliveryTo: 'Architect' }
   ]
 
   return (
